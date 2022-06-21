4e00f926b2ab5d8bcf3d186acfc7aae8a08af13d
iBabs <ibrahimbabalola8@gmail.com>
2022-06-21 23:07:56 +0100

    commit first change

---
+++ script.js
@@ -0,0 +1,29 @@
+// let number = prompt("Please enter a number")
+
+// if (number != null){
+
+//     alert("this is not a number")
+// }
+
+slcted_option = prompt("choose \n1 for addition,\n 2 for subtraction,\n 3 for multiplication: ")
+
+if (slcted_option == 1){
+                    (`you have selected ${slcted_option}`)
+                let numb1 = parseFloat (prompt("Enter a first number to be added: "))
+                let num2 = parseFloat (prompt(` ${numb1} plus: `))
+                let result = (numb1 + num2)
+                    alert(`your answer is ${result}`)}
+
+if (slcted_option == 2){
+                        (`you have selected ${slcted_option}`)
+                    let numb1 = parseFloat (prompt("Enter a first number to be subtracted: "))
+                    let num2 = parseFloat (prompt(` ${numb1} minus: `))
+                    let result = (numb1 - num2)
+                        alert(`your answer is ${result}`)}
+                        
+if (slcted_option == 3){
+                            (`you have selected ${slcted_option}`)
+                        let numb1 = parseFloat (prompt("Enter a first number to be multiplied: "))
+                        let num2 = parseFloat (prompt(` ${numb1} times: `))
+                        let result = (numb1 * num2)
+                            alert(`your answer is ${result}`)}
